d437b5f4aae93ac1a4cb216d6d07a4b1eb32a38f
DevMarques23 <ti.brunom@gmail.com>
2023-07-17 16:14:26 -0300

    Editando Settings, startando app e iniciando estrutura inicial

--- django_project_0/urls.py
+++ django_project_0/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('produtos/',include('produtos.urls')) # o produtos/ é o nome do acesso web, o produtos dentro do include  é o apontamento parameu app produtos
 ]
